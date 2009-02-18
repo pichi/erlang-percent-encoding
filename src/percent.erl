815e1ec212ea066c4457099ba30b4c83fd44c9ce
Tim Fletcher <tim@tfletcher.com>
2009-02-18 21:26:05 +0800

    Add is_alphanum macro.
    Signed-off-by: Hynek Vychodil <vychodil.hynek@gmail.com>

--- src/percent.erl
+++ src/percent.erl
@@ -2,6 +2,8 @@
 
 -export([url_encode/1, uri_encode/1, url_decode/1, uri_decode/1]).
 
+-define(is_alphanum(C), C >= $A, C =< $Z; C >= $a, C =< $z; C >= $0, C =< $9).
+
 %%
 %% Percent encoding as defined by the application/x-www-form-urlencoded
 %% content type (http://www.w3.org/TR/html4/interact/forms.html#h-17.13.4.1).
@@ -10,20 +12,15 @@
 url_encode(Str) when list(Str) ->
   url_encode(lists:reverse(Str, []), []).
 
-url_encode([X | T], Acc)
-  when  X >= $0, X =< $9;
-        X >= $a, X =< $z;
-        X >= $A, X =< $Z;
-        X == $-;
-        X == $_;
-        X == $. ->
+url_encode([X | T], Acc) when ?is_alphanum(X); X =:= $-; X =:= $_; X =:= $. ->
   url_encode(T, [X | Acc]);
 url_encode([32 | T], Acc) ->
   url_encode(T, [$+ | Acc]);
 url_encode([X | T], Acc) ->
   NewAcc = [$%, hexchr(X bsr 4), hexchr(X band 16#0f) | Acc],
   url_encode(T, NewAcc);
-url_encode([], Acc) -> Acc.
+url_encode([], Acc) ->
+  Acc.
 
 %%
 %% Percent encoding as defined by RFC 3986 (http://tools.ietf.org/html/rfc3986).
@@ -32,19 +29,13 @@ url_encode([], Acc) -> Acc.
 uri_encode(Str) when list(Str) ->
   uri_encode(lists:reverse(Str, []), []).
 
-uri_encode([X | T], Acc)
-  when  X >= $0, X =< $9;
-        X >= $a, X =< $z;
-        X >= $A, X =< $Z;
-        X == $-;
-        X == $_;
-        X == $.;
-        X == $~ ->
+uri_encode([X | T], Acc) when ?is_alphanum(X); X =:= $-; X =:= $_; X =:= $.; X =:= $~ ->
   uri_encode(T, [X | Acc]);
 uri_encode([X | T], Acc) ->
   NewAcc = [$%, hexchr(X bsr 4), hexchr(X band 16#0f) | Acc],
   url_encode(T, NewAcc);
-uri_encode([], Acc) -> Acc.
+uri_encode([], Acc) ->
+  Acc.
 
 %%
 %% Percent decoding.
